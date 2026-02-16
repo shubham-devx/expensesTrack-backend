50b3778fd0892a7b942f846b0e9ddaf87d362ebf
shubham <subhamv2003@gmail.com>
2026-02-16 17:37:12 +0530

    Added production API URL

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://expense-track-frontend-eight.vercel.app/"
+      "https://expense-track-frontend-eight.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
